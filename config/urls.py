b17b62d3afdef31e51a178268ad5bc93de985931
kallolpratim <ksaikia615@gmail.com>
2020-02-20 16:40:07 +0530

    about and service page

--- config/urls.py
+++ config/urls.py
@@ -7,15 +7,13 @@ from django.views import defaults as default_views
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-     path("services/", TemplateView.as_view(template_name="pages/service.html"), name="service"),
-    path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    ),
+    path("services/", TemplateView.as_view(template_name="pages/service.html"), name="service"),
+    path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
-    path("users/", include("browndome.users.urls", namespace="users")),
-    path("accounts/", include("allauth.urls")),
+    # path("users/", include("browndome.users.urls", namespace="users")),
+    # path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
